a15c0470bc761522a8c89b946ed51c9bfc0097e1
Bird <sharp.oussama@gmail.com>
2023-09-26 20:13:03 +0100

    style form and init yup validation

--- app/src/scenes/signup.jsx
+++ app/src/scenes/signup.jsx
@@ -1,15 +1,19 @@
-import { useFormik, validateYupSchema } from "formik";
+import { Formik } from "formik";
+import * as Yup from "yup";
 
 function Signup() {
     return (
-        <div className="flex flex-col justify-center w-2/3">
-            <h2 className="text-lg p-2 min-w-[5em] bg-secondary text-center rounded">Signup</h2>
+        <div className="flex flex-col justify-center w-1/3">
+            <h2 className="text-lg p-2 min-w-[5em] bg-secondary text-center rounded-tl rounded-tr">
+                Signup
+            </h2>
             <SignupForm />
         </div>
     );
 }
 
 function SignupForm() {
+    // eslint-disable-next-line no-unused-vars
     const validate = (values) => {
         const errors = {};
         if (!values.firstName) {
@@ -43,82 +47,92 @@ function SignupForm() {
         return errors;
     };
 
-    const formik = useFormik({
-        initialValues: {
-            firstName: "",
-            lastName: "",
-            email: "",
-            password: "",
-        },
-        validate,
-        onSubmit: (values) => {
-            console.log(values);
-        },
-    });
-
     return (
-        <form onSubmit={formik.handleSubmit}>
-            <div className="flex flex-col gap-3">
-                <div>
-                    {" "}
-                    <label htmlFor="firstName">First name</label>
-                    <input
-                        type="text"
-                        id="firstName"
-                        name="firstName"
-                        value={formik.values.firstName}
-                        onChange={formik.handleChange}
-                    />
-                    {formik.errors.firstName ? (
-                        <div>{formik.errors.firstName}</div>
-                    ) : null}
-                </div>
-                <div>
-                    {" "}
-                    <label htmlFor="lastName">Last name</label>
-                    <input
-                        type="text"
-                        name="lastName"
-                        id="lastName"
-                        value={formik.values.lastName}
-                        onChange={formik.handleChange}
-                    />
-                    {formik.errors.lastName ? (
-                        <div>{formik.errors.lastName}</div>
-                    ) : null}
-                </div>
-                <div>
-                    {" "}
-                    <label htmlFor="email">e-mail</label>
-                    <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        value={formik.values.email}
-                        onChange={formik.handleChange}
-                    />
-                    {formik.errors.email ? (
-                        <div>{formik.errors.email}</div>
-                    ) : null}
-                </div>
-                <div>
-                    {" "}
-                    <label htmlFor="password">password</label>
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        value={formik.values.password}
-                        onChange={formik.handleChange}
-                    />
-                    {formik.errors.password ? (
-                        <div>{formik.errors.password}</div>
-                    ) : null}
-                </div>
-            </div>
+        <>
+            <Formik
+                initialValues={{
+                    firstName: "",
+                    lastName: "",
+                    email: "",
+                    password: "",
+                }}
+                validationSchema={Yup.object({
+                    firstName: Yup.string().required("First name is required"),
+                    lastName: Yup.string().required("Last name is required"),
+                    email: Yup.string()
+                        .email("Not a valid email address")
+                        .required("Email is required"),
+                })}
+                onSubmit={(values) => {
+                    console.log(values);
+                }}
+            >
+                {(formik) => (
+                    <form className="bg-primary rounded-bl rounded-br p-4" onSubmit={formik.handleSubmit}>
+                        <div className="flex flex-col gap-3">
+                            <div>
+                                {" "}
+                                <label htmlFor="firstName">First name</label>
+                                <input
+                                    type="text"
+                                    id="firstName"
+                                    name="firstName"
+                                    value={formik.values.firstName}
+                                    onChange={formik.handleChange}
+                                />
+                                {formik.errors.firstName ? (
+                                    <div>{formik.errors.firstName}</div>
+                                ) : null}
+                            </div>
+                            <div>
+                                {" "}
+                                <label htmlFor="lastName">Last name</label>
+                                <input
+                                    type="text"
+                                    name="lastName"
+                                    id="lastName"
+                                    value={formik.values.lastName}
+                                    onChange={formik.handleChange}
+                                />
+                                {formik.errors.lastName ? (
+                                    <div>{formik.errors.lastName}</div>
+                                ) : null}
+                            </div>
+                            <div>
+                                {" "}
+                                <label htmlFor="email">e-mail</label>
+                                <input
+                                    type="email"
+                                    id="email"
+                                    name="email"
+                                    value={formik.values.email}
+                                    onChange={formik.handleChange}
+                                />
+                                {formik.errors.email ? (
+                                    <div>{formik.errors.email}</div>
+                                ) : null}
+                            </div>
+                            <div>
+                                {" "}
+                                <label htmlFor="password">password</label>
+                                <input
+                                    type="password"
+                                    id="password"
+                                    name="password"
+                                    value={formik.values.password}
+                                    onChange={formik.handleChange}
+                                />
+                                {formik.errors.password ? (
+                                    <div>{formik.errors.password}</div>
+                                ) : null}
+                            </div>
+                        </div>
 
-            <button type="submit">Sign up</button>
-        </form>
+                        <button type="submit">Sign up</button>
+                    </form>
+                )}
+            </Formik>
+        </>
     );
 }
 
