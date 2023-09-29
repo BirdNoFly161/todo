e360ba196e97f1daa7bb9ef196d08c45908ee110
Bird <sharp.oussama@gmail.com>
2023-09-29 20:36:21 +0100

    misc

---
+++ api/middleware/passport.js
@@ -0,0 +1,6 @@
+import {Strategy as JwtStrategy} from 'passport-jwt';
+console.log(JwtStrategy);
+
+export default function (app){
+    
+}
--- app/src/redux/user/userSlice.js
+++ app/src/redux/user/userSlice.js
@@ -2,13 +2,25 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
   user: null,
+  token: null,
 }
 
 export const counterSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
-
+    setUser: (state, action)=>{
+      state.user = action.payload;
+    },
+    clearUser: (state)=>{
+      state.user = null;
+    },
+    setAuthToken: (state, action)=>{
+      state.token = action.payload;
+    },
+    clearAuthToken: (state)=>{
+      state.token = null;
+    },
   },
 })
 
