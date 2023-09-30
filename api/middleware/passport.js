19956f9e59c9f5d3fc7b4430bf0b20506f430d34
Bird <sharp.oussama@gmail.com>
2023-09-30 17:21:49 +0100

    sync

--- api/middleware/passport.js
+++ api/middleware/passport.js
@@ -1,7 +1,9 @@
 import {Strategy as JwtStrategy, ExtractJwt} from 'passport-jwt';
+import {secret} from '../configs/environement.js';
 
 let options = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken,
+    secretOrKey: secret
 }
 
 export default function (app){
