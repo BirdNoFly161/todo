0d6f4c4392502d4e1de6ecf86ac63cd4fff184cf
BirdNoFly161 <sharp.oussama@gmail.com>
2023-10-06 01:59:28 +0100

    added date to tasks

--- app/src/scenes/task/addTask.jsx
+++ app/src/scenes/task/addTask.jsx
@@ -6,84 +6,105 @@ import Spinner from "../../components/spinner";
 import API from "../../../api";
 
 function AddTask({ open, setOpen }) {
-    const currentUser = useSelector((state) => state.user.currentUser);
+  const currentUser = useSelector((state) => state.user.currentUser);
 
-    return (
-        <Dialog
-            as="div"
-            className="fixed inset-0 flex justify-center items-center"
-            open={open}
-            onClose={() => setOpen(true)}
-        >
-            <Formik
-                initialValues={{
-                    title: "",
-                    //deadline: "",
-                }}
-                validationSchema={Yup.object({
-                    title: Yup.string().required("title is required"),
-                    //endDate: Yup.string().required("Password is required"),
-                })}
-                onSubmit={async (values, { setSubmitting }) => {
-                    values = { ...values, people: [currentUser._id] };
-                    let response = await API.post("/tasks", { task: values });
-                    response = await response.json();
-                    setSubmitting(false);
-                    console.log(response);
-                }}
-            >
-                {(formik) => (
-                    <form
-                        className="w-1/3 h-1/3 flex flex-col items-center gap-1 bg-primary rounded p-4"
-                        onSubmit={formik.handleSubmit}
-                    >
-                        <div className="flex flex-col gap-3 w-full">
-                            <div className="flex flex-col">
-                                {" "}
-                                <div className="flex items-center">
-                                    {" "}
-                                    <label
-                                        className="min-w-[6em] bg-secondary rounded-tl rounded-bl px-2 py-1"
-                                        htmlFor="title"
-                                    >
-                                        Title
-                                    </label>
-                                    <input
-                                        className="px-2 py-1 grow rounded-tr rounded-br"
-                                        type="text"
-                                        id="title"
-                                        name="title"
-                                        value={formik.values.title}
-                                        onBlur={formik.handleBlur}
-                                        onChange={formik.handleChange}
-                                    />
-                                </div>
-                            </div>
-                        </div>
+  return (
+    <Dialog
+      as="div"
+      className="fixed inset-0 flex justify-center items-center"
+      open={open}
+      onClose={() => setOpen(true)}
+    >
+      <Formik
+        initialValues={{
+          title: "",
+          deadline: "",
+        }}
+        validationSchema={Yup.object({
+          title: Yup.string().required("title is required"),
+          endDate: Yup.date(),
+        })}
+        onSubmit={async (values, { setSubmitting }) => {
+          values = { ...values, people: [currentUser._id] };
+          console.log("trying to add task with values: ", values);
+          let response = await API.post("/tasks", { task: values });
+          response = await response.json();
+          setSubmitting(false);
+          console.log(response);
+        }}
+      >
+        {(formik) => (
+          <form
+            className="max- w-1/3 max-h-1/3 flex flex-col items-center gap-1 bg-primary rounded p-4"
+            onSubmit={formik.handleSubmit}
+          >
+            <div className="flex flex-col gap-3 w-full">
+              <div className="flex flex-col">
+                {" "}
+                <div className="flex items-center">
+                  {" "}
+                  <label
+                    className="min-w-[6em] bg-secondary rounded-tl rounded-bl px-2 py-1"
+                    htmlFor="title"
+                  >
+                    Title
+                  </label>
+                  <input
+                    className="px-2 py-1 grow rounded-tr rounded-br"
+                    type="text"
+                    id="title"
+                    name="title"
+                    value={formik.values.title}
+                    onBlur={formik.handleBlur}
+                    onChange={formik.handleChange}
+                  />
+                </div>
+              </div>
+            </div>
 
-                        <div className="flex justify-center items-center gap-3">
-                            <button
-                                className="flex justify-center bg-secondary hover:scale-110 hover:shadow-surround hover:shadow-accent  hover:text-accent rounded px-2 py-1"
-                                type="submit"
-                            >
-                                {formik.isSubmitting ? (
-                                    <Spinner />
-                                ) : (
-                                    <span>Add task</span>
-                                )}
-                            </button>
-                            <button
-                                className="flex justify-center bg-secondary hover:scale-110 hover:shadow-surround hover:shadow-accent  hover:text-accent rounded px-2 py-1"
-                                onClick={() => setOpen(false)}
-                            >
-                                Cancel
-                            </button>
-                        </div>
-                    </form>
-                )}
-            </Formik>
-        </Dialog>
-    );
+            <div className="flex flex-col gap-3 w-full">
+              <div className="flex flex-col">
+                {" "}
+                <div className="flex items-stretch">
+                  {" "}
+                  <label
+                    className="min-w-[6em] bg-secondary rounded-tl rounded-bl px-2 py-1"
+                    htmlFor="deadline"
+                  >
+                    {"Due by (Optional)"}
+                  </label>
+                  <input
+                    className="px-2 grow rounded-tr rounded-br"
+                    type="date"
+                    id="deadline"
+                    name="deadline"
+                    value={formik.values.deadline}
+                    onBlur={formik.handleBlur}
+                    onChange={formik.handleChange}
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="flex justify-center items-center gap-3">
+              <button
+                className="flex justify-center bg-secondary hover:scale-110 hover:shadow-surround hover:shadow-accent  hover:text-accent rounded px-2 py-1"
+                type="submit"
+              >
+                {formik.isSubmitting ? <Spinner /> : <span>Add task</span>}
+              </button>
+              <button
+                className="flex justify-center bg-secondary hover:scale-110 hover:shadow-surround hover:shadow-accent  hover:text-accent rounded px-2 py-1"
+                onClick={() => setOpen(false)}
+              >
+                Cancel
+              </button>
+            </div>
+          </form>
+        )}
+      </Formik>
+    </Dialog>
+  );
 }
 
 export default AddTask;
--- app/src/scenes/task/index.jsx
+++ app/src/scenes/task/index.jsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from "react";
 import API from "../../../api";
 import { useSelector } from "react-redux";
-import {BiPlusCircle} from 'react-icons/bi'
+import { BiPlusCircle } from "react-icons/bi";
 import AddTask from "./addTask";
 
 function Tasks() {
   const currentUser = useSelector((state) => state.user.currentUser);
-  const [addTaskOpen, setAddTaskOpen] = useState(false)
+  const [addTaskOpen, setAddTaskOpen] = useState(false);
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
@@ -30,26 +30,32 @@ function Tasks() {
   }, []);
 
   return (
-    <div className="flex flex-col items-center gap-4">
-      <h1>Tasks</h1>
+    <div className="bg-accent flex flex-col items-center gap-4 border border-primary rounded p-5">
+      <span className="bg-secondary px-4 py-1 rounded">Tasks</span>
       {tasks.map((task, index) => (
-        <Task key={index} title={task.title} />
+        <Task key={index} {...task} />
       ))}
       <button
         className="text-lg flex justify-center items-center gap-3 p-2 min-w-[5em] bg-secondary rounded hover:scale-110 hover:shadow-surround hover:shadow-accent  hover:text-accent"
-        onClick={()=>setAddTaskOpen(true)}
+        onClick={() => setAddTaskOpen(true)}
       >
         <span>New task</span>
-        <BiPlusCircle/>
+        <BiPlusCircle />
       </button>
-      <AddTask open={addTaskOpen} setOpen={setAddTaskOpen}/>
+      <AddTask open={addTaskOpen} setOpen={setAddTaskOpen} />
     </div>
   );
 }
 
-function Task({ title }) {
+function Task({ title, deadline }) {
   return (
-    <div className="flex justify-between bg-secondary rounded">{title}</div>
+    <div className="flex justify-between px-4 py-2 gap-3 bg-primary rounded">
+      <span className="bg-secondary px-2 py-1">{title}</span>
+      <span className="flex items-stretch bg-secondary px-2 py-1">
+        <span className="px-2">Due by:</span>
+        <span className="bg-white px-2">{deadline}</span>
+      </span>
+    </div>
   );
 }
 
