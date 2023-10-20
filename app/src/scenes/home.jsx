99458dc9ff92b8f56b0c86d43aa98c39271f2c20
BirdNoFly161 <sharp.oussama@gmail.com>
2023-10-20 13:47:35 +0100

    removed unused components

--- app/src/scenes/home.jsx
+++ app/src/scenes/home.jsx
@@ -1,6 +1,4 @@
-import Checkmark from "../components/svg/checkmark";
 import Noteboard from "../components/svg/noteboard";
-import Test from "../components/Test";
 import { useState } from "react";
 
 function Home() {
