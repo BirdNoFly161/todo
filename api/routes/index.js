f9dd44d51c204131fbb73559fdb9995456721975
BirdNoFly161 <sharp.oussama@gmail.com>
2023-10-28 18:32:42 +0100

    added route

--- api/routes/index.js
+++ api/routes/index.js
@@ -6,4 +6,8 @@ router.get("/", function (req, res, next) {
   res.send("Express on vercel");
 });
 
+router.get("/r1", function (req, res, next) {
+  res.json({ msg: "Express on vercel" });
+});
+
 export default router;
