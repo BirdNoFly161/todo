ed4e33dda666959073a4b3f8d6c5c6ffc9ac6a57
BirdNoFly161 <sharp.oussama@gmail.com>
2023-10-29 17:14:33 +0100

    removed index.js and redirecting to /bin/www.js in vercel.json

--- api/app.js
+++ api/app.js
@@ -23,9 +23,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "https://oussamabenmansour.site",
-      "http://oussamabenmansour.site",
       "https://todo-app-sharpoussama-gmailcom.vercel.app",
-      "http://todo-app-sharpoussama-gmailcom.vercel.app",
     ],
     preflightContinue: true,
   }),
